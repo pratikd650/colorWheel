4d02c645cffa61c5acb7d1455f2672d56f2d0a4f
pratikd650 <pratikd650@gmail.com>
2016-02-06 01:05:54 -0800

    Update colorWheel.js

--- scripts/colorWheel.js
+++ scripts/colorWheel.js
@@ -1,1 +1,15 @@
 
+// https://github.com/FastLED/FastLED/blob/master/hsv2rgb.cpp
+function hsv2rgb_rainbow() {
+  
+}
+
+var ColorWheel = React.createClass({
+  render: function() {
+    var n = 12;
+    var colorDots = [];
+    
+    return (
+    );
+  }
+})

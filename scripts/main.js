fa47f4c87fc21ef8e083a4bcc1c09bfe5dd2d813
pratikd650 <pratikd650@gmail.com>
2016-02-11 11:59:00 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -324,9 +324,9 @@ var LeftRightArrow = React.createClass({
   render: function() {
     return(
       <div className="inline field">
-          <label className="ui small  label">{this.props.label}</label>
+          <label className="ui small">{this.props.label}</label>
           <button type="button" className="ui compact mini button" onClick={this.changeSpeed.bind(this, -1)}><i className="left chevron icon"></i></button> 
-          <label className="ui small label">{this.state.speed}</label>
+          <label className="ui small">{this.state.speed}</label>
           <button type="button" className="ui compact mini button" onClick={this.changeSpeed.bind(this, +1)}><i className="right chevron icon"></i></button>
       </div>);
   }
